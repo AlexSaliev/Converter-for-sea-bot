d4ce95ad5d3483ce0eadf86c390b72bc7d305058
AlexSaliev <alexsaliev93@gmail.com>
2020-03-29 15:17:26 +0200

    GUI this app
    балванка без логики с преобразованием формата файла

---
+++ converter.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+import pandas as pd
+
+root= tk.Tk()
+root.title('Конвертер для Никиты')
+canvas1 = tk.Canvas(root, width = 500, height = 300, bg = 'lightsteelblue2', relief = 'raised')
+canvas1.pack()
+
+label1 = tk.Label(root, text='Привет, Никита', bg = 'lightsteelblue2')
+label1.config(font=('helvetica', 20))
+canvas1.create_window(150, 60, window=label1)
+
+def getTxt ():
+    global read_file
+    
+    import_file_path = filedialog.askopenfilename()
+    label1 = tk.Label(root, text=import_file_path, bg = 'lightsteelblue2')
+    canvas1.create_window(150, 160, window=label1)
+    read_file = pd.read_csv(import_file_path)
+    
+browseButtonTxt = tk.Button(text="Выбери файл",width=18, command=getTxt, bg='green', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(150, 130, window=browseButtonTxt)
+
+def convertToCsv ():
+    global read_file
+    
+    export_file_path = filedialog.asksaveasfilename(defaultextension='.csv')
+    read_file.to_csv (export_file_path, index = None)
+
+saveAsButtonCsv = tk.Button(text='Преобразовать в CSV',width=18, command=convertToCsv, bg='green', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(150, 187, window=saveAsButtonCsv)
+
+def exitApplication():
+    MsgBox = tk.messagebox.askquestion ('Exit Application','Are you sure you want to exit the application',icon = 'warning')
+    if MsgBox == 'yes':
+       root.destroy()
+     
+exitButton = tk.Button (root, width=18, text='Закрыть',command=exitApplication, bg='brown', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(150, 270, window=exitButton)
+
+root.mainloop()
